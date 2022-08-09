a1f7072c0080806beec4366892b0837abea5eb9d
贺东 曾 <Zenghedong@outlook.com>
2022-08-09 20:50:37 +0800

    增加注释
    Signed-off-by: 贺东 曾 <Zenghedong@outlook.com>

--- opencv/颜色空间的转换.py
+++ opencv/颜色空间的转换.py
@@ -7,11 +7,14 @@ def callback():
     pass
 
 
+#创建窗口
 cv2.namedWindow("win", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("win", 640, 480)
-
+#读取图片
 img = cv2.imread("./1.jpg")
+#定义颜色空间列表
 color_space = [cv2.COLOR_BGR2BGRA, cv2.COLOR_BGR2RGB, cv2.COLOR_BGR2GRAY, cv2.COLOR_BGR2HSV, cv2.COLOR_BGR2YUV]
+#创建trackbar
 cv2.createTrackbar("bar", "win", 0, 4, callback)
 while True:
     index = cv2.getTrackbarPos("bar", "win")
