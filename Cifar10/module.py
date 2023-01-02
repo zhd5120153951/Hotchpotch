13f0d893acfc8f9c280682405ddf38abd5fd95bd
贺东 曾 <Zenghedong@outlook.com>
2023-01-02 12:19:30 +0800

    增加训练、测试、保存模型
    Signed-off-by: 贺东 曾 <Zenghedong@outlook.com>

---
+++ Cifar10/module.py
@@ -0,0 +1,89 @@
+from torch import nn
+import torch.nn.functional as F
+import torch
+
+
+class LeNetRGB(nn.Module):
+    def __init__(self):
+        super(LeNetRGB, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)   # 3表示输入是3通道
+        self.relu = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.maxpool2 = nn.MaxPool2d(2, 2)
+ 
+ 
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+ 
+ 
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.maxpool2(x)
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+    
+
+def train_runner(model, device, trainloader, optimizer, epoch, Loss, Accuracy):
+    #训练模型, 启用 BatchNormalization 和 Dropout, 将BatchNormalization和Dropout置为True
+    model.train()
+    total = 0
+    correct =0.0
+ 
+ 
+    #enumerate迭代已加载的数据集,同时获取数据和数据下标
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        #把模型部署到device上
+        inputs, labels = inputs.to(device), labels.to(device)
+        #初始化梯度
+        optimizer.zero_grad()
+        #保存训练结果
+        outputs = model(inputs)
+        #计算损失和
+        #多分类情况通常使用cross_entropy(交叉熵损失函数), 而对于二分类问题, 通常使用sigmod
+        loss = F.cross_entropy(outputs, labels)
+        #获取最大概率的预测结果
+        #dim=1表示返回每一行的最大值对应的列下标
+        predict = outputs.argmax(dim=1)
+        total += labels.size(0)
+        correct += (predict == labels).sum().item()
+        #反向传播
+        loss.backward()
+        #更新参数
+        optimizer.step()
+        if i % 100 == 0:
+            #loss.item()表示当前loss的数值
+            print("Train Epoch {} \t Loss: {:.6f}, accuracy: {:.6f}%".format(epoch, loss.item(), 100*(correct/total)))
+            Loss.append(loss.item())
+            Accuracy.append(correct/total)
+    return loss.item(), correct/total
+
+def test_runner(model, device, testloader):
+    #模型验证, 必须要写, 否则只要有输入数据, 即使不训练, 它也会改变权值
+    #因为调用eval()将不启用 BatchNormalization 和 Dropout, BatchNormalization和Dropout置为False
+    model.eval()
+    #统计模型正确率, 设置初始值
+    correct = 0.0
+    test_loss = 0.0
+    total = 0
+    #torch.no_grad将不会计算梯度, 也不会进行反向传播
+    with torch.no_grad():
+        for data, label in testloader:
+            data, label = data.to(device), label.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(output, label).item()
+            predict = output.argmax(dim=1)
+            #计算正确数量
+            total += label.size(0)
+            correct += (predict == label).sum().item()
+        #计算损失值
+        print("test_avarage_loss: {:.6f}, accuracy: {:.6f}%".format(test_loss/total, 100*(correct/total)))
