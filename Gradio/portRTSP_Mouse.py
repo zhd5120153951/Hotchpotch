338e9490fa2f7a42137ec7b83c79f327a6a9162a
zhd5120153951 <zenghedong@outlook.com>
2023-09-25 14:07:12 +0800

    gradio实现网页端口获取rtsp流区域入侵坐标,并保存到json中
    Signed-off-by: zhd5120153951 <zenghedong@outlook.com>

--- Gradio/portRTSP_Mouse.py
+++ Gradio/portRTSP_Mouse.py
@@ -1,8 +1,7 @@
-from tkinter import NO
 import cv2
 import gradio as gr
 import numpy as np
-from sympy import true
+import json
 
 
 #处理rtsp和mouse事件
@@ -12,19 +11,33 @@ def process_rtsp_mouse(rtsp_url, output):
 
     #定义鼠标回调函数--尝试在外部定义
     def mouse_callback(event, x, y, flag, param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            with open("./Gradio/" + output, 'a') as file:
-                file.write(f"{x},{y}\n")
-
-    cv2.namedWindow("rtsp")
-    cv2.resizeWindow("rtsp", 1920, 1080)
-    cv2.setMouseCallback("rtsp", mouse_callback)
+        global points, Polygon_point, count
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            polygon = zip(Polygon_point, points)
+            with open("./Gradio/" + output, 'w') as f:
+                json.dump(dict(polygon), f)
+            # with open("./Gradio/" + output, 'a') as f:
+            #     f.write(f"{x},{y}\n")
+        elif event == cv2.EVENT_LBUTTONDOWN:
+            count += 1
+            points.append((x, y))
+            pt = 'pt' + str(count)
+            Polygon_point.append(pt)
+        elif event == cv2.EVENT_RBUTTONDOWN:
+            points = []
+            Polygon_point = []
 
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-        cv2.imshow("rtsp", frame)
+        temp_frame = frame.copy()
+        if len(points) > 0:
+            cv2.polylines(temp_frame, np.array([points]), isClosed=True, color=(0, 255, 0), thickness=1)
+        cv2.namedWindow("rtsp")
+        cv2.resizeWindow("rtsp", 1920, 1080)
+        cv2.imshow("rtsp", temp_frame)
+        cv2.setMouseCallback("rtsp", mouse_callback)
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
 
@@ -35,4 +48,8 @@ def process_rtsp_mouse(rtsp_url, output):
 iface = gr.Interface(fn=process_rtsp_mouse, inputs=["text", "text"], outputs=None, title="rtsp stream")
 
 if __name__ == "__main__":
+    points = []
+    Polygon_point = []
+    count = 0
+    output = None
     iface.launch()
