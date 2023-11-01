452a77777993f355cbaaa77e3a135dedd0a3a6e7
贺东 曾 <Zenghedong@outlook.com>
2023-11-01 20:23:21 +0800

    pc端向远程服务器post图像
    Signed-off-by: 贺东 曾 <Zenghedong@outlook.com>

--- SE5/se5_deployment/fire/python/post.py
+++ SE5/se5_deployment/fire/python/post.py
@@ -9,21 +9,20 @@ def img2base64(img_path):
         # 将图片内容进行base64编码
         encoded_image = base64.b64encode(image_file.read())
 
-        return encoded_image.decode("utf-8")#把byte转换为字符串
+        return encoded_image.decode("utf-8")  #把byte转换为字符串
 
 
 if __name__ == '__main__':
-    encoded_string = img2base64("./3.jpg")
+    encoded_string = img2base64("E:\\Github\\Hotchpotch\\SE5\\se5_deployment\\fire\\python\\27.jpg")
     # 打印编码后的图片内容
     # print(encoded_string)
     data = {'device': 'Dell-G15', 'ratio': 1.2, 'img_path': encoded_string}
     header = {"Content-Type": "application/json; charset=utf-8"}
     # print(json.dumps(data))
     try:
-        requests.post("http://127.0.0.1:5002/flask", json.dumps(data), headers=header)
+        ret = requests.post("http://8.137.53.211/prod-api/system/role/list?pageNum=1&pageSize=10&status=0",
+                            json.dumps(data),
+                            headers=header)
+        print(ret)
     except Exception as e:
         print(e)
-
-
-
-
