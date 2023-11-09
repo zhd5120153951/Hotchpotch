b8380754e2bbfda55b74c093197ad6c3bd79684a
贺东 曾 <Zenghedong@outlook.com>
2023-11-09 22:03:38 +0800

    python实现和远程互联网服务器get,post数据
    Signed-off-by: 贺东 曾 <Zenghedong@outlook.com>

--- Flask/SE5_Flask/client.py
+++ Flask/SE5_Flask/client.py
@@ -1,8 +1,42 @@
 import requests
 
-file_path1 = './python/27.jpg'  # 图片路径
-img = open(file_path1, 'rb')
-res = {"file": img}
-# 访问服务
-res = requests.post("http://192.168.22.4:6666", files=res)  # 如 http://152.111.111.11:6666
-print(res.text)
+
+if __name__ == '__main__':
+
+    # LABELS_URL = 'http://8.137.53.211//dev-api/system/student'
+
+    # classes = {
+    #     (key, value) for (key, value)
+    #     in requests.get(LABELS_URL).json().items()
+    # }
+
+    # print(classes)
+
+    #  替换为您想要获取数据的远程服务器URL
+    url = "http://8.137.53.211/prod-api/system/product/123"
+    # data = dict()
+    #  发送GET请求
+    response = requests.get(url)
+    # print(data)
+    #  检查请求是否成功
+    if response.status_code == 200:
+        #  获取服务器返回的数据
+        data = response.json()
+        print("获取到的数据：",  data)
+    else:
+        print("请求失败，状态码：",  response.status_code)
+
+    # file_path1 = './1.jpg'  # 图片路径
+    # img = open(file_path1, 'rb')
+    # res = {"file": img}
+    # data = {'key1': 'value1', 'key2': 'value2'}
+    # # 访问服务
+    # # 如 http://152.111.111.11:6666
+    # # res = requests.post("http://8.137.53.211//dev-api/system/role", files=res)
+    # ret = requests.post("http://8.137.53.211//dev-api/system/role", data=res)
+
+    # print(ret.text)
+    # if ret.status_code == 200:
+    #     print('数据发送成功')
+    # else:
+    #     print('数据发送失败')
--- SE5/se5_deployment/fire/python/post.py
+++ SE5/se5_deployment/fire/python/post.py
@@ -8,19 +8,21 @@ def img2base64(img_path):
     with open(img_path, 'rb') as image_file:
         # 将图片内容进行base64编码
         encoded_image = base64.b64encode(image_file.read())
-
-        return encoded_image.decode("utf-8")  #把byte转换为字符串
+        return encoded_image.decode("utf-8")  # 把byte转换为字符串
 
 
 if __name__ == '__main__':
-    encoded_string = img2base64("E:\\Github\\Hotchpotch\\SE5\\se5_deployment\\fire\\python\\27.jpg")
+    encoded_string = img2base64(
+        "E:\\Github\\Hotchpotch\\SE5\\se5_deployment\\fire\\python\\27.jpg")
     # 打印编码后的图片内容
     # print(encoded_string)
-    data = {'device': 'Dell-G15', 'ratio': 1.2, 'img_path': encoded_string}
+    # data = {'device': 'Dell-G15', 'ratio': 1.2, 'img_path': encoded_string}
+    data = {'device': 'Dell-G15', 'ratio': 1.2, 'img_path': 'this is img'}
+
     header = {"Content-Type": "application/json; charset=utf-8"}
     # print(json.dumps(data))
     try:
-        ret = requests.post("http://8.137.53.211/prod-api/system/role/list?pageNum=1&pageSize=10&status=0",
+        ret = requests.post("http://8.137.53.211//dev-api/system/role",
                             json.dumps(data),
                             headers=header)
         print(ret)
