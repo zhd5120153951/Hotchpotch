68ebf6a3e9f94bbf12e2aa71765c670fe9672360
zhd5120153951 <zenghedong@outlook.com>
2024-02-02 08:35:08 +0800

    删除无关文件
    Signed-off-by: zhd5120153951 <zenghedong@outlook.com>

--- Flask_Vue_Deploy_Yolo/flask_js_yolov5/back-end/core/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .resnet50 import resnet50
-
-get_model_from_name = {
-    "resnet50"      : resnet50,
-}
--- Flask_Vue_Deploy_Yolo/flask_vue_yolov5/front-end/declaration.d.ts
+++
@@ -1,6 +0,0 @@
-/* 创建时间 2023-3-11 */
-declare module '“vue-json-viewer”' {
-    const vis: any;
-    export default vis;
-}
-
